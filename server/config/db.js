8f556e0b809f7cd5a3c4818b245e48fb91af0f6c
MarkJaven <185623926+MarkJaven@users.noreply.github.com>
2026-01-22 11:28:48 +0800

    fix supabase connection

--- server/config/db.js
+++ server/config/db.js
@@ -7,13 +7,16 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     ssl: {
       require: true,
       rejectUnauthorized: false
-    }
+    },
+    statement_timeout: 30000,
+    idle_in_transaction_session_timeout: 30000
   },
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
+    idle: 10000,
+    evict: 15000
   }
 });
 
